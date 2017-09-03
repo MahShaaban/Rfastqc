7805ac4e40a2481a5003e58db732126a3a808417
MahShaaban <mahmoud.s.fahmy@students.kasralainy.edu.eg>
2017-09-03 10:03:33 +0900

    added plot method

--- R/methods.R
+++ R/methods.R
@@ -36,3 +36,44 @@ test_get <- function(fqc, test = NULL) {
             vars(2:ncol(test_dat)),
             function(x) as.numeric(x))
 }
+
+#' Plot method for FastQC objects
+#'
+#' @inheritParams test_get
+#' @param ... Other arguments to pass to \code{link[graphics]{plot}}
+#'
+#' @return A line graph.
+#' @importFrom graphics lines axis
+#' @export
+plot <- function(fqc, test = NULL, ...) {
+  UseMethod('plot')
+}
+
+#' @export
+plot.FastQC <- function(fqc, test = NULL, ...) {
+  if(is.null(test)) {
+    stop('User Should provied a test name.')
+  } else if(test == 'Basic Statistics') {
+    stop('Nothing to plot. Use test_get instead.')
+  } else if(test == 'summary') {
+    stop('Nothing to plot. Use summary instead.')
+  }
+  tst_dat <- test_get(fqc, test)
+  x <- tst_dat[, 1]
+  if(test %in% c('Per base sequence content', 'Adapter Content')) {
+    y <- tst_dat[, 2:ncol(tst_dat)]
+  } else {
+    y  <- tst_dat[, 2]
+  }
+  if(is.null(dim(y))) {
+    plot(x, y, type = 'n', xaxt = 'n', main = test, ...)
+    lines(x, y, ...)
+    axis(1, at = 1:length(x), labels = x)
+  } else if(ncol(y) > 1) {
+    plot(x, y[, 1], type = 'n', xaxt = 'n', main = test, ...)
+    axis(1, at = 1:length(x), labels = x)
+    for(i in 1:ncol(y)) {
+      lines(x, y = y[,i], ...)
+    }
+  }
+}
--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -12,3 +12,13 @@ test_that('test_get', {
   expect_true(is.factor(tst_dat$Base))
   expect_true(all(apply(tst_dat[, -1], 2, is.numeric)))
 })
+
+test_that('plot', {
+  fqc <- parse_fqc('sample_1.txt')
+  fqc <- FastQC(fqc)
+  nms <- names(fqc)
+  expect_error(plot(fqc), 'User Should provied a test name.')
+  expect_error(plot(fqc, test = nms[1]), 'Nothing to plot. Use test_get instead.')
+  expect_error(plot(fqc, test = nms[10]), 'Nothing to plot. Use summary instead.')
+  expect_silent(plot(fqc, test = nms[2]))
+})
